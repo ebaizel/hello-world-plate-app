87d59735598dabbaefedad5e57d952ea1882a917
Emile Baizel <ebaizel@gmail.com>
2013-11-25 10:36:44 -0800

    Initial Commit

---
+++ Plate/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  Plate
+//
+//  Created by emileleon on 11/25/13.
+//  Copyright (c) 2013 Plate SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Plate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Plate
+//
+//  Created by emileleon on 11/25/13.
+//  Copyright (c) 2013 Plate SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLAppDelegate class]));
+    }
+}
